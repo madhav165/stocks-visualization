e850f0c5d6449b45e85aa37badf55561dce35054
Madhav Kandukuri <madhav165@gmail.com>
2016-07-02 14:13:07 +0530

    Now test.py displays a graph

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 import get_stocks as gs
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib import style
 import datetime as dt
 import numpy as np
 
@@ -14,15 +15,30 @@ def bytesupdate2num(fmt, encoding='utf-8'):
     return bytesconverter
 
 def read_csv(file_name):
-   print ('Reading %s' % file_name)
-   date, openp, highp, lowp, closep, volume, adjclosep = np.loadtxt(file_name, 
+    print ('Reading %s' % file_name)
+    date, openp, highp, lowp, closep, volume, adjclosep = np.loadtxt(file_name, 
                                                                     delimiter=',', 
                                                                     unpack=True,
                                                                     converters={0:bytesupdate2num('%Y-%m-%d')},
                                                                     skiprows=1
-                                                                   )
+                                                                    )
+    return date, openp, highp, lowp, closep, volume, adjclosep
+
+def plot_data(symbol, date, openp, highp, lowp, closep, volume, adjclosep):
+    style.use('ggplot')
+    ax1 = plt.subplot2grid((1,1), (0,0), rowspan=1, colspan=1)
+    plt.plot(date,adjclosep, label='Adjusted closing price')
+    plt.ylabel('Adjusted Closing Price')
+    plt.title(symbol)
+    #plt.legend()
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    for label in ax1.xaxis.get_ticklabels():
+        label.set_rotation(45)
+    plt.subplots_adjust(left=0.09,bottom=0.15,right=0.94,top=0.92,wspace=0.2, hspace=0)
+    plt.show()
 
 if __name__=='__main__':
     symbol=gs.get_going()
     file_name=symbol+'.csv'
-    read_csv(file_name)
+    date, openp, highp, lowp, closep, volume, adjclosep=read_csv(file_name)
+    plot_data(symbol, date, openp, highp, lowp, closep, volume, adjclosep)
